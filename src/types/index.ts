030eb6a9d8050ddab2b2aea5890c50ff7508d8b0
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-23 18:07:02 -0300

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,61 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    cards?: Card[];
+}
+
+export interface Card {
+    id: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    createdAt: string;
+}
+
+export interface PaginatedResponse<T> {
+    list: T[];
+    rpp: number;
+    page: number;
+    more: boolean;
+}
+
+export type TradeType = 'OFFERING' | 'RECEIVING';
+
+export interface TradeCard {
+    id: string;
+    cardId: string;
+    tradeId: string;
+    type: TradeType;
+    card: Card;
+}
+
+export interface Trade {
+    id: string;
+    userId: string;
+    createdAt: string;
+    user: {
+        name: string;
+    };
+    tradeCards: TradeCard[];
+}
+
+export interface LoginResponse {
+    token: string;
+    user: Omit<User, 'cards'>;
+}
+
+export interface RegisterResponse {
+    userId: string;
+}
+
+export interface CreateTradePayload {
+    cards: {
+        cardId: string;
+        type: TradeType;
+    }[];
+}
+
+export interface CreateTradeResponse {
+    tradeId: string;
+}
