93860f131990e08f12bfe2817dbffc6518ba42a5
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-25 11:54:35 -0300

    fix: use reactive values for auto fetch

--- src/composables/useTrades.ts
+++ src/composables/useTrades.ts
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/vue-query";
 import { TradesService } from "@/services/trades.service";
+import type { MaybeRef } from "vue";
+import { toValue } from "vue";
 
-export const useTrades = (page: number, rpp: number) => {
+export const useTrades = (page: MaybeRef<number>, rpp: MaybeRef<number>) => {
   return useQuery({
     queryKey: ['trades', page, rpp],
-    queryFn: () => TradesService.getAll(page, rpp),
+    queryFn: () => TradesService.getAll(toValue(page), toValue(rpp)),
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 10
   })
