663fd8e3c17a780bc96a56dcb8f466f2931937c5
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-25 11:55:54 -0300

    fix: remove unecessary class

--- src/components/layout/BaseLayout.vue
+++ src/components/layout/BaseLayout.vue
@@ -5,8 +5,8 @@ import NavigationBar from '@/components/layout/NavigationBar.vue'
 <template>
   <NavigationBar />
 
-  <div class="min-h-screen bg-gray-50 text-gray-900 font-sans flex flex-col overflow-x-hidden">
-    <main class="grow container mx-auto px-5 py-6 bg-neutral-50">
+  <div class="min-h-screen bg-slate-100 text-gray-900 font-sans flex flex-col overflow-x-hidden">
+    <main class="grow container mx-auto px-5 py-6">
       <slot />
     </main>
 
