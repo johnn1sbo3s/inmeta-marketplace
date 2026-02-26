96f5b99626e457c0712d43aa2d5e35359668a3a5
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-02-25 22:44:44 -0300

    feat: implement trades page

---
+++ src/components/ui/PageHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-0.5">
+    <h1 class="text-4xl font-semibold">
+      {{ title }}
+    </h1>
+
+    <p class="text-sm text-muted">
+      {{ description }}
+    </p>
+  </div>
+</template>
---
+++ src/pages/TradesPage.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import BaseLayout from '@/components/layout/BaseLayout.vue';
+import PageHeader from '@/components/ui/PageHeader.vue';
+import Button from '@/components/ui/BaseButton.vue';
+import { useInfiniteTrades } from '@/composables/useInfiniteTrades';
+
+const rpp = ref(10)
+
+const {
+  data: tradesData,
+  isLoading: tradesIsLoading,
+  isFetchingNextPage,
+  hasNextPage,
+  fetchNextPage
+} = useInfiniteTrades(rpp)
+
+const items = computed(() => {
+  if (!tradesData.value) return []
+  return tradesData.value.pages.flatMap(page => page.list)
+})
+
+const computedTradesLoading = computed(() => {
+  return tradesIsLoading.value && !tradesData.value
+})
+
+</script>
+
+<template>
+  <BaseLayout>
+    <div class="flex flex-col gap-4 pb-8">
+      <PageHeader
+        title="Propostas"
+        description="Confira as propostas disponíveis"
+      />
+
+      <div class="bg-white p-5 border border-neutral-100 rounded-xl flex flex-col gap-4 items-center">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 w-full justify-center">
+          <template v-if="computedTradesLoading">
+            <USkeleton
+              v-for="item in 6"
+              :key="item"
+              class="h-[170px] w-full bg-slate-200"
+            />
+          </template>
+
+          <TradeCard
+            v-for="item in items"
+            :key="item.id"
+            :item="item"
+          />
+        </div>
+
+        <div
+          v-if="hasNextPage"
+          class="flex justify-center mt-4"
+        >
+          <Button
+            variant="soft"
+            color="primary"
+            size="lg"
+            :loading="isFetchingNextPage"
+            @click="fetchNextPage()"
+          >
+            Carregar mais
+          </Button>
+        </div>
+      </div>
+    </div>
+  </BaseLayout>
+</template>
